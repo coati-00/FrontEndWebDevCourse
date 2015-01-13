703ee0068e3b9b9085faa96d689b2acbd203f2ef
Cassandra Dunlop <cdulop@columbia.com>
2015-01-13 13:44:36 -0500

    update

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
 
   //when user wishes to add and item to the todo list...
   $('.add-item-section input[type=text]').on('keydown', function(event)
-  {  console.log(event.which);
+  {  //console.log(event.which);
      if ( event.which === 13 ) 
      {
        event.preventDefault();
-       console.log($(this).val())
+       //console.log($(this).val());
+       var temp_val = $(this).val();
+       if(temp_val === '')
+       {
+           //console.log("there is nothing to add");
+           //console.log();
+           $('#item-entry').append("<p>Please enter something to add to the list</p>");
+           return;
+       }
        $('.to-buy-section ul').append('<li><input type="checkbox" />' + $(this).val() + '</li>');
        $(this).val("");
      }
